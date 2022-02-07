95e5c907e7cf38fc1fbe0759f2b1d18cc2eb657d
Stefan Inman <96351972+Anon-67@users.noreply.github.com>
2022-02-07 11:46:05 -0600

    commit tree 69e328c088c91d7fc7bfc50030017c082ef9269c

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let test = document.getElementById('main');
+
 function addingEventListener() {
-}
+    document.getElementById('input').addEventListener('click', function() {
+        alert("I was clicked");
+    });
+};
+
+
+
+
+
+addingEventListener(test);
